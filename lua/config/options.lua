703e5b1c6ac72a1d797b852e4e5e87bc7b6bf120
willvernon <wmvernon@proton.me>
2023-09-06 10:07:39 -0400

    add- Copilot
    Adding Copilot and lualine update not sure if lualine is added correctly for copilot.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,8 +5,5 @@ vim.opt.winbar = '%=%m %f'
 vim.opt.cursorline = false
 -- -- Set the color of line numbers to yellow
 -- vim.cmd("highlight LineNr guifg=grey ctermfg=grey")
--- -- Set the color of comments to green
-vim.cmd('highlight Keyword guifg=grey ctermfg=grey')
-vim.cmd(
-  [[highlight Keyword term=bold cterm=bold gui=underline guifg=#FF0000 ctermfg=red]]
-)
+vim.opt.wrap = true
+vim.opt.linebreak = true
