7ab8b6d332263bdaa7d8a4741b29424579b021a6
willvernon <wmvernon@proton.me>
2023-09-06 09:03:16 -0400

    sonokai and window maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set(
-	"n",
-	"<leader>sx",
-	require("telescope.builtin").resume,
-	{ noremap = true, silent = true, desc = "Resume" }
+  'n',
+  '<leader>sx',
+  require('telescope.builtin').resume,
+  { noremap = true, silent = true, desc = 'Resume' }
 )
+-- Remap keys for switching windows
+vim.keymap.set('n', '<leader>ww', '<C-w>w')
+vim.keymap.set('n', '<leader>wh', '<C-w>h')
+vim.keymap.set('n', '<leader>wk', '<C-w>k')
+vim.keymap.set('n', '<leader>wj', '<C-w>j')
+vim.keymap.set('n', '<leader>wl', '<C-w>l')
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,4 +6,7 @@ vim.opt.cursorline = false
 -- -- Set the color of line numbers to yellow
 -- vim.cmd("highlight LineNr guifg=grey ctermfg=grey")
 -- -- Set the color of comments to green
--- vim.cmd("highlight Comment guifg=grey ctermfg=grey")
+vim.cmd('highlight Keyword guifg=grey ctermfg=grey')
+vim.cmd(
+  [[highlight Keyword term=bold cterm=bold gui=underline guifg=#FF0000 ctermfg=red]]
+)
