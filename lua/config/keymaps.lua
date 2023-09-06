97ec540c6ffa7f6e4691b10d81265c739ca5e181
willvernon <wmvernon@proton.me>
2023-09-06 11:28:27 -0400

    Add- Copilot

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,6 @@ vim.keymap.set('n', '<leader>wh', '<C-w>h')
 vim.keymap.set('n', '<leader>wk', '<C-w>k')
 vim.keymap.set('n', '<leader>wj', '<C-w>j')
 vim.keymap.set('n', '<leader>wl', '<C-w>l')
+-- Needed for markdown
+vim.opt.wrap = true
+vim.opt.linebreak = true
