71c8d7c27e789546d017d31d4ba44c58dd14a634
willvernon <wmvernon@proton.me>
2023-09-06 12:06:09 -0400

    add- keymaps
    Still need to find out how to remap copilot and lsp keymap to tab

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,5 +14,18 @@ vim.keymap.set('n', '<leader>wk', '<C-w>k')
 vim.keymap.set('n', '<leader>wj', '<C-w>j')
 vim.keymap.set('n', '<leader>wl', '<C-w>l')
 -- Needed for markdown
-vim.opt.wrap = true
-vim.opt.linebreak = true
+-- vim.opt.wrap = true
+-- vim.opt.linebreak = true
+-- Toggle for linebreak
+local Util = require('lazyvim.util')
+vim.keymap.set('n', '<leader>ub', function()
+  Util.toggle('linebreak')
+end, { desc = 'Toggle linebreak' })
+-- LSP Suggestion Prompt Accept with Tab
+-- Doesnt seem to work
+-- vim.api.nvim_set_keymap(
+--   'i',
+--   '<Tab>',
+--   'pumvisible() ? "<C-n>" : "<Tab>"',
+--   { expr = true }
+-- )
